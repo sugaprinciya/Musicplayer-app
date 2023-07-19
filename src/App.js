5c688fcb58872204ef8bd8e66c8f2da9593514e7
sugaprinciya <sugaprinciyathiraviyam@gmail.com>
2023-07-19 20:11:51 +0530

    musicapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+//import Styles
+import "../src/Style/app.scss";
+//import Components
+import Player from "../src/Component/Player";
+import Song from "../src/Component/Song";
+import Library from "../src/Component/Library";
+import Nav from "../src/Component/Nav";
+//import data
+import chillHop from "./data";
 
-function App() {
+export default function App() {
+  //Ref
+  const audioRef = useRef(null);
+  //State
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [repeat, setRepeat] = useState(false);
+  const volumeStorage = () =>
+    localStorage.getItem("volume") ? localStorage.getItem("volume") : 1;
+
+  const [volume, setVolume] = useState(JSON.parse(volumeStorage()));
+  const [libraryStatus, setLibraryStatus] = useState(false);
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    //claculate percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
+  };
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if (isPlaying) audioRef.current.play();
+  };
+  const setSongVolume = () => {
+    audioRef.current.volume = volumeStorage();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <div clasName="header_s" style={{ backgroundColor: "#9aabe1fa" }}>
+        <Nav
+          libraryStatus={libraryStatus}
+          setLibraryStatus={setLibraryStatus}
+        />
+        <Song currentSong={currentSong} isPlaying={isPlaying} />
+        <Player
+          setSongVolume={setSongVolume}
+          volume={volume}
+          setVolume={setVolume}
+          audioRef={audioRef}
+          setIsPlaying={setIsPlaying}
+          isPlaying={isPlaying}
+          currentSong={currentSong}
+          setSongInfo={setSongInfo}
+          songInfo={songInfo}
+          songs={songs}
+          setCurrentSong={setCurrentSong}
+          setSongs={setSongs}
+          setRepeat={setRepeat}
+          repeat={repeat}
+        />
+        <Library
+          audioRef={audioRef}
+          songs={songs}
+          setCurrentSong={setCurrentSong}
+          isPlaying={isPlaying}
+          setSongs={setSongs}
+          libraryStatus={libraryStatus}
+          currentSong={currentSong}
+        />
+        <audio
+          onLoadedMetadataCapture={setSongVolume}
+          onTimeUpdate={timeUpdateHandler}
+          onLoadedMetadata={timeUpdateHandler}
+          ref={audioRef}
+          src={currentSong.audio}
+          onEnded={songEndHandler}
+        ></audio>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,93 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Darshana",
+      cover:
+        "https://img.freepik.com/free-vector/silhouette-excited-audience-colourful-music-notes-background_1048-14332.jpg?w=740&t=st=1689761956~exp=1689762556~hmac=888b0020bd76296ba3a8f79d73dd8b98f7691549ed28a1b99201e09a366c5107",
+      // "https://chillhop.com/wp-content/uploads/2020/09/09fb436604242df99f84b9f359acb046e40d2e9e-1024x1024.jpg",
+      artist: "Hesham Abdul Wahab, Darshana Rajendran",
+      audio: "https://paglasongs.com/files/download/id/4036",
+      //    "https://mp3.chillhop.com/serve.php/?mp3=10148",
+      color: ["#956E8D", "#EFA099"],
+      id: uuidv4(),
+      active: "true",
+    },
+    {
+      name: "why this kolaveridi",
+      cover:
+        "https://img.freepik.com/free-psd/party-social-media-template_505751-2931.jpg?w=740&t=st=1689762250~exp=1689762850~hmac=645508518aac56698d6ab702d1a34c711b62fb8335605c9a7d2201abf1d2fb43",
+      // "https://chillhop.com/wp-content/uploads/2020/07/ad7fc4dda66ba986466fd2b1c416b0b12411ee28-1024x1024.jpg",
+      artist: "Dhanush",
+      audio: "https://paglasongs.com/files/download/id/13109",
+      //   "https://mp3.chillhop.com/serve.php/?mp3=8127",
+      color: ["#62573E", "#546F6E"],
+      id: uuidv4(),
+      active: "false",
+    },
+    {
+      name: "Unstoppable",
+      cover:
+        "https://img.freepik.com/free-psd/neon-vertical-poster-template-electronic-music-with-female-dj_23-2148979678.jpg?w=740&t=st=1689762350~exp=1689762950~hmac=03b36409390708c2075f93b728c97c5dd3b8aae97f1b3ecdcfc87c3617183b74",
+      // "https://chillhop.com/wp-content/uploads/2020/07/be9e5dca9d4c9015c407a2d2590af8a0e5e278ce-1024x1024.jpg",
+      artist: "	Sia",
+      audio: "https://paglasongs.com/files/download/id/4110",
+      //   "https://mp3.chillhop.com/serve.php/?mp3=8105",
+      color: ["#FABC8F", "#66C094"],
+      id: uuidv4(),
+      active: "false",
+    },
+    {
+      name: "Stereo Heart",
+      cover:
+        "https://img.freepik.com/free-photo/black-background-with-musical-note-it_1340-28217.jpg?t=st=1689762685~exp=1689766285~hmac=2793072a344dd4c1467fdfe55a089071afae3064eb1e81b0d81fe01869596c23&w=740",
+      // "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
+      artist: "Adam Levine.",
+      audio: "https://paglasongs.com/files/download/id/2680",
+      //   "https://mp3.chillhop.com/serve.php/?mp3=7955",
+      color: ["#5B4B99", "#FCE7E6"],
+      id: uuidv4(),
+      active: "false",
+    },
+    {
+      name: "Jalebi baby",
+      cover:
+        "https://img.freepik.com/free-psd/explore-music-flyer-template_23-2148986007.jpg?w=740&t=st=1689762832~exp=1689763432~hmac=874ab526c5de6efe3b1bcd8191bdd4b65eb76b98a12cb0642bac05cdf3c26a12",
+      // "https://chillhop.com/wp-content/uploads/2020/07/edaf1202cf33c7a2a122b7494a7e4d3e643af45f-1024x1024.jpg",
+      artist: "	Tesher, Neha Sharma",
+      audio: "https://paglasongs.com/files/download/id/2752",
+      //   "https://mp3.chillhop.com/serve.php/?mp3=9322",
+      color: ["#F5BD5C", "#B3506D"],
+      id: uuidv4(),
+      active: "false",
+    },
+    {
+      name: "Tere vaaste",
+      cover:
+        "https://img.freepik.com/free-psd/music-shop-poster-template_23-2148855476.jpg?w=740&t=st=1689762893~exp=1689763493~hmac=d3a69953e59aa06392089b813aec400d01ede8558ecf3a52f50e4f9e6ce67ca0",
+      // "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      artist: "Varun Jain, Shadab Faridi, Altamash Faridi",
+      audio: "https://paglasongs.com/files/download/id/14424",
+      //    "https://mp3.chillhop.com/serve.php/?mp3=8266",
+      color: ["#E4C3BE", "#F8B091"],
+      id: uuidv4(),
+      active: "false",
+    },
+    {
+      name: "Baby",
+      cover:
+        "https://img.freepik.com/free-psd/club-dj-party-flyer-social-media-post_505751-3166.jpg?w=740&t=st=1689762967~exp=1689763567~hmac=302584c0ce75771c21d1b14b7b41f9e89e2c0fbf5794eacc92b7188570600415",
+      // "https://chillhop.com/wp-content/uploads/2020/07/58028b4ad62050072228e4c833916e5512772399-1024x1024.jpg",
+      artist: "justin fiber",
+      //   "Blue Wednesday, Aso",
+      audio: "https://paglasongs.com/files/download/id/2576",
+      //    "https://mp3.chillhop.com/serve.php/?mp3=9048",
+      color: ["#392934", "#E1858F"],
+      id: uuidv4(),
+      active: "false",
+    },
+  ];
+}
+
+export default chillHop;
